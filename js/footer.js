86265b65a95ad06f11600984d8895c0913ae56c0
alejopujals <alejopujals@gmail.com>
2024-05-17 00:12:39 -0300

    Header y footer responsive

--- js/footer.js
+++ js/footer.js
@@ -1,9 +1,9 @@
 // ----- Creo la variable miFooter y pegamos el código del HMTL -----
 let miFooter = `
-<nav class="">
-        <a href="">Preguntas frecuentes <i class="fa-solid fa-question"></i></a>
-        <a href="mailto:info@amigosdevillaluro.com.ar"">Escribinos <i class="fa-regular fa-envelope"></i></a>
-        <a href="">Llamanos <i class="fa-solid fa-phone"></i></a>
+<nav class="footer">
+        <a href=""><span class="texto_footer">Preguntas frecuentes </span><i class="fa-solid fa-question"></i></a>
+        <a href="mailto:info@amigosdevillaluro.com.ar""><span class="texto_footer">Escribinos </span><i class="fa-regular fa-envelope"></i></a>
+        <a href=""><span class="texto_footer">Llamanos </span><i class="fa-solid fa-phone"></i></a>
         <a href="https://www.facebook.com/AmigosDeVillaLuro/?locale=es_LA" target="_blank"><i class="fa-brands fa-facebook"></i></a>
         <a href="https://www.instagram.com/asocamigosdevillaluro/?hl=es" target="_blank"><i class="fa-brands fa-instagram"></i></a>
 </nav>
--- js/header.js
+++ js/header.js
@@ -1,10 +1,10 @@
 // ----- Creo la variable miHeader y pegamos el código del HMTL -----
 let miHeader = `
-<nav class="">
+<nav class="header">
             <a href="./index.html">Noticias</a>
             <a href="./quiénes somos.html">Quiénes somos</a>
             <a href="./deportes.html">Deportes</a>
-            <a href="./formulario.html" target="_blank" class="btn-inscripcion" id="logo">Inscribite &#9997;</a>
+            <a href="./formulario.html" target="_blank" class="btn-inscripcion" id="logo"><span id="texto_btn_inscripcion">Inscribite </span>&#9997;</a>
 </nav>
 `
 
